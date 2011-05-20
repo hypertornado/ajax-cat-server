189882e188d79d51d68a93f306049aba1e52dc76
hypertornado <hypertornado@gmail.com>
2011-05-20 16:00:07 +0200

    changes for bachelor work

--- run_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SERVICE='ajax-cat-server'
- 
-if [ `ps -C $SERVICE | wc -l` -eq 2 ]; then
-  echo "Server is running. Everything is OK.";
-else
-  echo "Server is not running. Starting server.";
-  ./$SERVICE
-fi
